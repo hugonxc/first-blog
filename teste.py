db9dd869230d0d72aa5d27b47521e0d8ddad5aa5
Hugo Neves de Carvalho <hugo-nc@hotmail.com>
2016-08-16 21:46:02 -0300

    Django girls app, initial commit

---
+++ teste.py
@@ -0,0 +1,15 @@
+a = 5 > 1
+b = 1 > 5
+
+participante = {'nome': 'Hugo', 'var1': a, 'var2': b}
+
+print(participante['var1'])
+
+def testing(name):
+    print ('Hi '+ name + '!')
+
+girls = ['Laissa', 'Maria', 'Kelen', 'Japa']
+
+for i in range(0,4):
+    testing(girls[i])
+    print('\n')
